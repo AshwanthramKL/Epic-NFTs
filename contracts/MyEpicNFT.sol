6ed8f825e3ce4b5e3147305a6074b7e25bec2da5
“[Martialeagle <k.l.ashwanthram@gmail.com>
2022-07-13 12:38:00 +0530

    added mint functionality

---
+++ contracts/MyEpicNFT.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+contract MyEpicNFT is ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenId; // Counter for the no. of NFTs
+
+    constructor() ERC721 ("SquareNFT","SQR") { // NFT name and NFT symbol
+        console.log("This is my Contract. Whoa!");
+    }
+
+    // Mint an NFT
+    function makeanEpicNFT() public {           
+        uint256 newItemId = _tokenId.current(); // temporary variable to get current NFT id
+
+        _safeMint(msg.sender, newItemId);   // Mint NFT
+
+        _setTokenURI(newItemId, "data");    // Set NFT id and metadata
+
+        _tokenId.increment();   // Increase count of NFT
+
+    }
+}
